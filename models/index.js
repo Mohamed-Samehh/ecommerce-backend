76b4fe2d0d3e4b8e3d378c499da149e887b7be0a
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-02-21 15:27:58 +0200

    Added Auth & User Logic

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ const Authors = require('./authors');
 const Books = require('./books');
 const Cart = require('./Cart');
 const Category = require('./Category');
+const User = require('./user');
 
 module.exports = {
   Books,
   Authors,
   Category,
-  Cart
+  Cart,
+  User
 };
---
+++ models/user.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  dob: {
+    type: Date,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  roles: {
+    type: [String],
+    default: ['user']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.pre('findOneAndUpdate', async function (next) {
+  const update = this.getUpdate();
+  if (update && update.password) {
+    const hashed = await bcrypt.hash(update.password, 10);
+    this.setUpdate({...update, password: hashed});
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
