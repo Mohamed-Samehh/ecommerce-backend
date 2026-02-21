3486fb32c84078c3493e61052016243ca96bb593
AlaaAbdullah13 <alaaabdallah33339@gmail.com>
2026-02-21 23:01:20 +0200

    Finish: Order transactions, stock update, and review eligibility logic

--- models/index.js
+++ models/index.js
@@ -2,10 +2,16 @@ const Authors = require('./authors');
 const Books = require('./books');
 const Cart = require('./Cart');
 const Category = require('./Category');
+const Order = require('./order');
+const OrderItem = require('./order-item');
+const Review = require('./review');
 
 module.exports = {
   Books,
   Authors,
   Category,
-  Cart
+  Cart,
+  Order,
+  Review,
+  OrderItem
 };
--- routes/orders.js
+++ routes/orders.js
@@ -2,17 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 const {orderController} = require('../controllers');
+const {protect, restrictTo} = require('../middleware/auth-middleware');
 const validateRequest = require('../middleware/validate-request');
 const {orderSchema, statusSchema, paymentSchema} = require('../utils/validations');
+
+router.use(protect);
 
 router.post('/', validateRequest(orderSchema), orderController.placeOrder);
 
 router.get('/my', orderController.getMyOrders);
 
-router.get('/', orderController.getAllOrders);
+router.get('/', restrictTo('admin'), orderController.getAllOrders);
 
-router.patch('/:id/status', validateRequest(statusSchema), orderController.updateOrderStatus);
+router.patch('/:id/status', restrictTo('admin'), validateRequest(statusSchema), orderController.updateOrderStatus);
 
-router.patch('/:id/payment', validateRequest(paymentSchema), orderController.updatePaymentStatus);
+router.patch('/:id/payment', restrictTo('admin'), validateRequest(paymentSchema), orderController.updatePaymentStatus);
 
 module.exports = router;
