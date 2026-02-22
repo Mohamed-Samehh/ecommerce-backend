92d93ffca95e7535b29d8e74471bafbc2e4d8734
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-22 15:16:05 +0200

    fix: bugfix cart controllers

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,22 +1,27 @@
 const {Cart, Book} = require('../models');
 
 async function add(req, res, next) { // Post /cart (logged in user)
-  const {_id} = req.user; // get user from token
-  const {bookId, quantity} = req.body; // validated by Joi
+  const {id} = req.user; // get user from token
+  const {bookId} = req.body; // validated by Joi
+  const quantity = Number(req.body.quantity);
   try {
     const myBook = await Book.findById(bookId); // check book exists in DB
+
     if (!myBook)
       return res.status(404).send({status: 'fail', message: 'Book not found'});
 
-    if (myBook.stock < quantity) // check stock
-      return res.status(400).send({status: 'fail', message: 'Not enough stock'});
+    const myCart = await Cart.findOneAndUpdate({user: id}, {}, {upsert: true, new: true}); // find user's cart or create new one
 
-    const myCart = await Cart.findOneAndUpdate({user: _id}, {}, {upsert: true, new: true}); // find user's cart or create new one
     const bookInCart = myCart.items.find((item) => bookId === item.book.toString()); // check if book already in cart
 
     if (!bookInCart) {
+      if (myBook.stock < quantity) // if new book => check stock
+        return res.status(400).send({status: 'fail', message: 'Not enough stock'});
+
       myCart.items.push({book: bookId, quantity});
     } else {
+      if (myBook.stock < bookInCart.quantity + quantity) // if not new => check quant. of book in cart and desired quant.
+        return res.status(400).send({status: 'fail', message: 'Not enough stock'});
       bookInCart.quantity += quantity;
     }
     await myCart.save();
@@ -28,9 +33,9 @@ async function add(req, res, next) { // Post /cart (logged in user)
 }
 
 async function display(req, res, next) { // Get /cart (logged in user)
-  const {_id} = req.user;
+  const {id} = req.user;
   try {
-    const userBooks = await Cart.findOne({user: _id}).populate('items.book'); // search by user ref. & use populate to show book details for frontend
+    const userBooks = await Cart.findOne({user: id}).populate('items.book'); // search by user ref. & use populate to show book details for frontend
     if (!userBooks) return res.status(200).send({status: 'success', data: {items: []}});
 
     res.status(200).send({status: 'success', data: userBooks}); // items array contain each book details and quantity
@@ -40,11 +45,12 @@ async function display(req, res, next) { // Get /cart (logged in user)
 }
 
 async function remove(req, res, next) { // Delete /cart/:bookId (logged in user)
-  const {_id} = req.user;
+  console.log('in remove');
+  const {id} = req.user;
   const {bookId} = req.params;
 
   try {
-    const myCart = await Cart.findOne({user: _id});
+    const myCart = await Cart.findOne({user: id});
     if (!myCart) {
       return res.status(404).send({status: 'fail', message: 'Cart not found'});
     }
@@ -64,8 +70,42 @@ async function remove(req, res, next) { // Delete /cart/:bookId (logged in user)
   }
 }
 
+// 5. Find book in cart → 404 if not found
+// 6. Check stock >= new quantity
+// 7. Replace quantity
+// 8. Save + return 200
+
+async function update(req, res, next) {
+  try {
+    const {id} = req.user;
+    const {bookId} = req.params;
+    const quantity = Number(req.body.quantity);
+
+    const myCart = await Cart.findOne({user: id}); // find user's cart
+    if (!myCart) return res.status(404).send({status: 'fail', message: 'Cart not found'});
+
+    const bookInCart = myCart.items.find((item) => bookId === item.book.toString()); // check if book already in cart
+
+    if (!bookInCart) {
+      return res.status(404).send({status: 'fail', message: 'Book not found in cart'});
+    } else {
+      const myBook = await Book.findById(bookId); // check book exists in DB
+
+      if (myBook.stock < quantity) // new quantity must not exceed stock
+        return res.status(400).send({status: 'fail', message: 'Not enough stock'});
+      bookInCart.quantity = quantity; // replace old quantity with new value
+    }
+    await myCart.save();
+
+    res.status(200).send({status: 'success', data: myCart});
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   add,
   display,
-  remove
+  remove,
+  update
 };
--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const auth = require('../middleware/auth');
 const validateRequest = require('../middleware/validate-request');
-const {cartSchema} = require('../utils/validations');
+const {cartSchema, updateCartSchema} = require('../utils/validations');
 
 const router = express.Router();
 const {cartController} = require('../controllers');
 
 router.get('/', auth, cartController.display); // view cart (logged in user)
 router.post('/', auth, validateRequest(cartSchema), cartController.add); // add book to cart (same )
+router.patch('/:bookId', auth, validateRequest(updateCartSchema), cartController.update); // to change existing book quantity
 router.delete('/:bookId', auth, cartController.remove); // delete book from cart (same)
 
 module.exports = router;
+
+// delete is called by frontend when quantity =0
