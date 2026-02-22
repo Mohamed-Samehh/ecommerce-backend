dc0845c32d04214327a4a73a4c944fc9ab40c00f
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-02-22 09:25:51 +0200

    llink auth middlewares to cart and category

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 const {cartController} = require('../controllers'); // TODO: Link from sameh
 
-router.get('/', cartController.display); // view cart (logged in user)
-router.post('/', cartController.add); // add book to cart (same )
-router.delete('/:bookId', cartController.remove); // delete book from cart (same)
+router.get('/', auth, cartController.display); // view cart (logged in user)
+router.post('/', auth, cartController.add); // add book to cart (same )
+router.delete('/:bookId', auth, cartController.remove); // delete book from cart (same)
 
 module.exports = router;
--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const isAdmin = require('../middleware/admin'); // then, check the req.user.roles
+const auth = require('../middleware/auth'); // first set the req.user
 
 const router = express.Router();
-const {categoryController} = require('../controllers'); // TODO: Link from sameh
+const {categoryController} = require('../controllers');
 
 router.get('/', categoryController.display); // list all categories (anyone)
-router.post('/', categoryController.create); // add category (admin)
-router.delete('/:id', categoryController.remove); // delete cat. (admin)
-router.patch('/:id', categoryController.update); // update cat. name (admin)
+router.post('/', auth, isAdmin, categoryController.create); // add category (admin)
+router.delete('/:id', auth, isAdmin, categoryController.remove); // delete cat. (admin)
+router.patch('/:id', auth, isAdmin, categoryController.update); // update cat. name (admin)
 module.exports = router;
+
+// req with no token => 401 error, else auth set req.user
+// req with no admin token (check req.user.roles)=> 403 error, else to the controller
