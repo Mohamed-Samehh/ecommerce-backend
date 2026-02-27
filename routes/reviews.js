80aab5ddc464674ad1532e734c2f432140c362dc
AlaaAbdullah13 <alaaabdallah33339@gmail.com>
2026-02-27 22:58:29 +0200

    feat: add getAllReviews endpoint and update review-related configurations

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const {reviewController} = require('../controllers');
+const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 const validateRequest = require('../middleware/validate-request');
 const {reviewSchema} = require('../utils/validations');
 
 router.get('/book/:bookId', reviewController.getBookReviews);
+router.get('/', auth, admin, reviewController.getAllReviews);
 
 router.post('/', auth, validateRequest(reviewSchema), reviewController.addReview);
 router.get('/my', auth, reviewController.getMyReviews);
