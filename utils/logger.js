4b895caa6fdb7b08c918d8c05aee361eb7677e65
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-28 22:09:02 +0200

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,27 +1,33 @@
+const process = require('node:process');
 const pino = require('pino');
+
+const targets = [
+  {
+    target: 'pino-pretty',
+    level: 'info',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname'
+    }
+  }
+];
+
+if (!process.env.VERCEL) {
+  targets.push({
+    target: 'pino/file',
+    level: 'error',
+    options: {
+      destination: 'logs/app.log',
+      mkdir: true
+    }
+  });
+}
 
 const logger = pino({
   level: 'info',
   transport: {
-    targets: [
-      {
-        target: 'pino-pretty',
-        level: 'info',
-        options: {
-          colorize: true,
-          translateTime: 'SYS:standard',
-          ignore: 'pid,hostname'
-        }
-      },
-      {
-        target: 'pino/file',
-        level: 'error',
-        options: {
-          destination: 'logs/app.log',
-          mkdir: true
-        }
-      }
-    ]
+    targets
   }
 });
 
