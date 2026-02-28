54eba0541b877873712c411e1271ad3d85735930
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-28 22:30:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const errorHandler = require('./utils/error-handler');
 const logger = require('./utils/logger');
-require('dotenv').config({ path: './config/.env' });
+require('dotenv').config({path: './config/.env'});
 const routes = require('./routes');
-const { deleteFromCloudinary } = require('./utils/cloudinary-handler');
+const {deleteFromCloudinary} = require('./utils/cloudinary-handler');
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:4200' }));
+app.use(cors({origin: process.env.FRONTEND_URL || 'http://localhost:4200'}));
 app.use(express.static('public'));
 app.use(express.json());
 
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
     deleteFromCloudinary(req.public_id);
   }
   if (handler) {
-    const { statusCode, ...body } = handler.handler(err);
+    const {statusCode, ...body} = handler.handler(err);
     logger.error(err);
 
     return res.status(statusCode).json(body);
@@ -44,18 +44,22 @@ app.use((err, req, res, next) => {
   }
 
   logger.error(err);
-  res.status(500).json({ status: 'error', message: 'Something went wrong' });
+  res.status(500).json({status: 'error', message: 'Something went wrong'});
 });
 app.use((req, res) => {
   res.sendStatus(404);
 });
 
+let isConnected = false;
+
 async function connectDB() {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
     logger.info('MongoDB Connected...');
   } catch (err) {
-    logger.error({ err }, 'Connection error');
+    logger.error({err}, 'Connection error');
     process.exit(1);
   }
 }
@@ -65,3 +69,5 @@ connectDB().then(() => {
     logger.info(`Server running on port ${process.env.PORT || 3000}`);
   });
 });
+
+module.exports = app;
