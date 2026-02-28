392588b9344e22390f85bd65f7dbc0604a898457
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-28 22:23:32 +0200

    Revert "Update logger.js"
    This reverts commit 4b895caa6fdb7b08c918d8c05aee361eb7677e65.

--- utils/logger.js
+++ utils/logger.js
@@ -1,33 +1,27 @@
-const process = require('node:process');
 const pino = require('pino');
-
-const targets = [
-  {
-    target: 'pino-pretty',
-    level: 'info',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname'
-    }
-  }
-];
-
-if (!process.env.VERCEL) {
-  targets.push({
-    target: 'pino/file',
-    level: 'error',
-    options: {
-      destination: 'logs/app.log',
-      mkdir: true
-    }
-  });
-}
 
 const logger = pino({
   level: 'info',
   transport: {
-    targets
+    targets: [
+      {
+        target: 'pino-pretty',
+        level: 'info',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname'
+        }
+      },
+      {
+        target: 'pino/file',
+        level: 'error',
+        options: {
+          destination: 'logs/app.log',
+          mkdir: true
+        }
+      }
+    ]
   }
 });
 
