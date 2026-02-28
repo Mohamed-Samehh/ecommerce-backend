6cf0ad71c2108b93acc7bfc1290042dbe735e7fc
“Mohamed <mohamed.kholy2011@gmail.com>
2026-02-28 22:23:37 +0200

    Revert "Update index.js"
    This reverts commit a285ec9e0a47da31cd38f1e87a387339a1caf31b.

--- index.js
+++ index.js
@@ -4,36 +4,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const errorHandler = require('./utils/error-handler');
 const logger = require('./utils/logger');
-require('dotenv').config({path: './config/.env'});
+require('dotenv').config({ path: './config/.env' });
 const routes = require('./routes');
-const {deleteFromCloudinary} = require('./utils/cloudinary-handler');
+const { deleteFromCloudinary } = require('./utils/cloudinary-handler');
 
 const app = express();
 
-app.use(cors({origin: process.env.FRONTEND_URL || 'http://localhost:4200'}));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:4200' }));
 app.use(express.static('public'));
 app.use(express.json());
-
-let connectionPromise;
-
-async function connectDB() {
-  if (mongoose.connection.readyState === 1) {
-    return;
-  }
-
-  if (!connectionPromise) {
-    connectionPromise = mongoose.connect(process.env.MONGO_URI)
-      .then(() => {
-        logger.info('MongoDB Connected...');
-      })
-      .catch((err) => {
-        connectionPromise = null;
-        throw err;
-      });
-  }
-
-  await connectionPromise;
-}
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
@@ -41,15 +20,6 @@ app.use((req, res, next) => {
     console.log('Body:', JSON.stringify(req.body, null, 2));
   }
   next();
-});
-
-app.use(async (req, res, next) => {
-  try {
-    await connectDB();
-    next();
-  } catch (err) {
-    next(err);
-  }
 });
 
 app.use(routes);
@@ -60,7 +30,7 @@ app.use((err, req, res, next) => {
     deleteFromCloudinary(req.public_id);
   }
   if (handler) {
-    const {statusCode, ...body} = handler.handler(err);
+    const { statusCode, ...body } = handler.handler(err);
     logger.error(err);
 
     return res.status(statusCode).json(body);
@@ -74,23 +44,24 @@ app.use((err, req, res, next) => {
   }
 
   logger.error(err);
-  res.status(500).json({status: 'error', message: 'Something went wrong'});
+  res.status(500).json({ status: 'error', message: 'Something went wrong' });
 });
 app.use((req, res) => {
   res.sendStatus(404);
 });
 
-if (!process.env.VERCEL) {
-  connectDB()
-    .then(() => {
-      app.listen(process.env.PORT || 3000, () => {
-        logger.info(`Server running on port ${process.env.PORT || 3000}`);
-      });
-    })
-    .catch((err) => {
-      logger.error({err}, 'Connection error');
-      process.exit(1);
-    });
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    logger.info('MongoDB Connected...');
+  } catch (err) {
+    logger.error({ err }, 'Connection error');
+    process.exit(1);
+  }
 }
 
-module.exports = app;
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    logger.info(`Server running on port ${process.env.PORT || 3000}`);
+  });
+});
